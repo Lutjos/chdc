d2f3296246de2f396019aa7a41057d2a9542bb3d
gmk0 <mkgeorges63@gmail.com>
2024-08-13 14:48:34 +0100

    save

---
+++ app/Livewire/Agent/DashboardAgent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Agent;
+
+use Livewire\Component;
+
+class DashboardAgent extends Component
+{
+    public function render()
+    {
+        return view('livewire.agent.dashboard-agent');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', \App\Livewire\Home\Home::class);
 
 
+
 Route::get('/dashboard',\App\Livewire\Dashboard::class);
 
 Route::get('/adminstration/utilisateurs/nouveau', \App\Livewire\Admin\FormAssignRole::class)
@@ -16,6 +17,19 @@ Route::get('/adminstration/utilisateurs',\App\Livewire\Admin\AssignRole::class)-
 
 
 
+
+Route::middleware([
+    'auth:sanctum',
+    'role:Agent',
+    config('jetstream.auth_session'),
+])->group( function(){
+
+    Route::prefix('agent')->group(function(){
+
+        Route::get('/dashboard',\App\Livewire\Agent\DashboardAgent::class);
+
+    });
+});
 
 Route::middleware([
     'auth:sanctum',
